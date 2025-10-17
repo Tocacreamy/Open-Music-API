2cd15734afe2a5ebdce1d45785cbf3f8d8b34262
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-10-17 13:43:22 +0700

    revision

--- src/validator/albums/schema.js
+++ src/validator/albums/schema.js
@@ -2,5 +2,5 @@ import Joi from "joi";
 
 export const AlbumPayloadSchema = Joi.object({
     name: Joi.string().required(),
-    year: Joi.number().required()
+    year: Joi.number().min(1900).max(new Date().getFullYear()).required()
 })
