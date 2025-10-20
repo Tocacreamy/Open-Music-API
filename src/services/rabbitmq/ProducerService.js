6cabb883cd51eb04871eeb061746774170fd9d4c
Tocacreamy <zekobaharudinfirdaus@gmail.com>
2025-10-20 15:13:33 +0700

    feat: implement ProducerService for RabbitMQ message handling

---
+++ src/services/rabbitmq/ProducerService.js
@@ -0,0 +1,14 @@
+import amqp from "amqplib";
+
+export const ProducerService = {
+  sendMessage: async (queue, message) => {
+    const connection = await amqp.connect(process.env.RABBITMQ_SERVER);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queue, { durable: true });
+    await channel.sendToQueue(queue, Buffer.from(message));
+
+    setTimeout(() => {
+      connection.close();
+    }, 1000);
+  },
+};
